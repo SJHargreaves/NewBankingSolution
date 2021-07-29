57eada82b58b224572db9240eaeb5c913c163fe6
SJHargreaves <87990644+SJHargreaves@users.noreply.github.com>
2021-07-29 10:57:58 -0400

    Initial commit

---
+++ BankingTests/BankAccountTests/NewAccounts.cs
@@ -0,0 +1,31 @@
+﻿
+using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace BankingTests
+{
+    public class NewAccounts
+    {
+        [Fact]
+        public void NewAccountsHaveCorrectBalance()
+        {
+            // Given (arrange)
+            var account = new BankAccount();
+
+            // When (act)
+            decimal openingBalance = account.GetBalance();
+            // Then (act)
+            Assert.Equal(5000M, openingBalance);
+        }
+
+      
+    }
+
+    
+}
---
+++ BankingTests/BankAccountTests/OverdraftNotAllowed.cs
@@ -0,0 +1,51 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BankingTests.BankAccountTests
+{
+    public class OverdraftNotAllowed
+    {
+
+        // balance cannot go below zero.
+        // account.Withdraw(1000000);
+        // - no money is taken from your account.
+
+        [Fact]
+        public void OverdraftDoesNotDecreaseBalance()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+
+            try
+            {
+                account.Withdraw(openingBalance + 1);
+            }
+            catch (OverdraftNotAllowedException)
+            {
+
+                // Gulp! I'm kind of expecting this, but I want to make sure we
+                // didn't decrement the balance on overdraft.
+            }
+
+            Assert.Equal(openingBalance, account.GetBalance());
+        }
+
+        // we should throw an exception.
+
+        [Fact]
+        public void OverdraftThrowsException()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+
+            Assert.Throws<OverdraftNotAllowedException>(
+                () => account.Withdraw(openingBalance + 1)
+                );
+        }
+    }
+}
